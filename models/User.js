cd8f99de554ee950e5e76392fb112f7905d34dcf
saravanan-22 <saravanan.skillsafari@gmail.com>
2023-09-10 13:51:41 +0530

    update_userprofileimage

--- models/User.js
+++ models/User.js
@@ -2,6 +2,10 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
   {
+    profileImage : {
+      type : String,
+      required : true,
+    },
     username: {
       type: String,
       required: true,
